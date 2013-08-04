965437c6afcf62425e5408f3dfec77a23794c06c
Eric Angle <angle@Erics-MacBook-Pro.local>
2013-08-03 18:40:55 -0600

    Initial Commit

---
+++ random/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  random
+//
+//  Created by Eric Angle on 8/3/13.
+//  Copyright (c) 2013 Eric Angle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
